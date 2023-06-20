29e3d58169152b56ab1e3aa4846d74c94d342310
denpero <peralta.den91@gmail.com>
2023-06-20 00:12:07 -0600

    Commit localstorage funcionando, falta limpiar el formulario y la tabla

--- js/principal.js
+++ js/principal.js
@@ -2,32 +2,21 @@
 const formElement = document.querySelector('#formulario');
 
 
-/* 7471851975 */
+
 
 var id = 0;
-let pacientesList = [];
+//let pacientesList = [];
+let pacientesList = localStorage.getItem('pacientesList') ? JSON.parse(localStorage.getItem('pacientesList')) : [];
+
 formElement.addEventListener('submit', (event) => {
 
     // prevenir el envio del formulario
     event.preventDefault();
+    
     id++;
     const radiosList = event.target.doctor;
     const statusElemnt = Array.from(radiosList).find(element => element.checked);
-    
-    /* const checkList = event.target.tipoPaciente.checked;
-
-    if(checkList){
-        console.log("si");
-    }else{
-        console.log("no");
-    } */
-
-    var nombre = event.target.nombre.value;
-    var phone = event.target.phone.value;
-    var motivo = event.target.motivo.value;
-    var fecha = event.target.datepicker.value;
-    var hora = event.target.timepicker.value;
-    var tipoPaciente = event.target.tipoPaciente.value;
+    // armar una estructura de datos, segun lo que necesitamos
     const pacientes = {
         id:id,
         nombre: event.target.nombre.value,
@@ -38,31 +27,43 @@ formElement.addEventListener('submit', (event) => {
         doctor:statusElemnt.value,
         tipoPaciente: event.target.tipoPaciente.checked,
     }
-
-
-
+    pacientesList.push(pacientes);
     //pacientesList = JSON.parse(localStorage.getItem("pacientesList"));
 
-    pacientesList.push(pacientes);
-   /*  pacientesList.push({
-        id: id,
-        nombre: nombre,
-        phone: phone,
-        motivo: motivo,
-        fecha: fecha,
-        hora: hora,
-        doctor: doctor,
-        tipoPaciente: tipoPaciente,
-    }); */
-    //  localStorage.setItem("pacientesList");
+    // limpiar la vista anterior
+    cleanView();
+    //localStorage.setItem("pacientesList", JSON.stringify(pacientesList));
+    localStorage.setItem('pacientesList', JSON.stringify(pacientesList));
 
-
-    console.log(pacientesList);
-
-    /*  */
-
-
+    /* var regresaList= localStorage.getItem('pacientesList');
+    var aux=JSON.parse(regresaList);
+      console.log(aux); */
+    renderViewlanguages(pacientesList);
 });
+
+
+const cleanView = () => {
+    formulario.innerHTML = '';
+};
+/* 7471851975 */
+const  renderViewlanguages = (pacientesList) =>{
+    pacientesList.forEach((element,index)=>{
+        console.log(element['nombre']);
+        $('.table').append('<tr><td>' + element['nombre'] +
+        '</td><td>' + element['fecha'] + '</td><td>' +
+        element['hora'] + '</td><td>' +
+        '\
+      <a href="#edit=' +element['id'] + '"data-target="modal1" class="btn-floating waves-effect waves-light orange btn modal-trigger hoverable"><i class="material-icons">edit</i>\
+          </a> \
+      <a href="#delete=' + element['id'] + '" id="btn-floating.red" name="btn-floating.red" class="btn-floating waves-effect waves-light red hoverable"><i class="material-icons">delete</i>\
+          </a> \
+                           </td></tr>')
+    })
+
+}
+if(pacientesList.length){
+    renderViewlanguages(pacientesList);
+}
 
 function salir() {
     Swal.fire({
@@ -78,79 +79,6 @@ function salir() {
     })
 }
 
-var data = [{
-    "id": 1,
-    "Name": "Kamba",
-    "Phone": "30-(541)656-1685",
-    "DTMF": 757
-}, {
-    "id": 2,
-    "Name": "Feedmix",
-    "Phone": "967-(362)975-4248",
-    "DTMF": 198
-}, {
-    "id": 3,
-    "Name": "Thoughtstorm",
-    "Phone": "358-(619)930-2339",
-    "DTMF": 252
-}, {
-    "id": 4,
-    "Name": "Shufflebeat",
-    "Phone": "86-(776)437-7364",
-    "DTMF": 689
-}, {
-    "id": 5,
-    "Name": "Reallinks",
-    "Phone": "55-(689)180-3162",
-    "DTMF": 173
-}, {
-    "id": 6,
-    "Name": "Digitube",
-    "Phone": "1-(504)256-2986",
-    "DTMF": 799
-}, {
-    "id": 7,
-    "Name": "Nlounge",
-    "Phone": "62-(928)582-6766",
-    "DTMF": 477
-}, {
-    "id": 8,
-    "Name": "Aimbu",
-    "Phone": "33-(573)429-4209",
-    "DTMF": 445
-}, {
-    "id": 9,
-    "Name": "Mydo",
-    "Phone": "370-(167)136-2174",
-    "DTMF": 854
-}, {
-    "id": 10,
-    "Name": "Tagcat",
-    "Phone": "46-(159)429-8509",
-    "DTMF": 859
-}];
-/*  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal1').modal();
-   
-    Materialize.updateTextFields();
-  }); */
-/* 
-Dynamic creation of table is not the best practice...
-Better way to clone existing table and fill it with data.
-*/
-$(data).each(function (i, elem) {
-    $('.table').append('<tr><td>' + elem['Name'] +
-        '</td><td>' + elem['Phone'] + '</td><td>' +
-        elem['DTMF'] + '</td><td>' +
-        '\
-      <a href="#edit=' + elem['id'] + '"data-target="modal1" class="btn-floating waves-effect waves-light orange btn modal-trigger hoverable"><i class="material-icons">edit</i>\
-          </a> \
-      <a href="#delete=' + elem['id'] + '" class="btn-floating waves-effect waves-light red hoverable"><i class="material-icons">delete</i>\
-          </a> \
-                           </td></tr>')
-});
-
 $('.btn-floating.orange').on('click', function () {
     console.log('Orange');
     $('#modal1').modal('open');
@@ -158,29 +86,34 @@ $('.btn-floating.orange').on('click', function () {
     var td = $(this).parents('tr').find('td:lt(3)');
     // $td.each(function(i){
     // Only the $() makes this td Object of DOM
-    $('#name').val($(td[0]).text());
+    $('#nombre').val($(td[0]).text());
     $('#phone').val($(td[1]).text());
-    $('#dtmf').val($(td[2]).text());
+    $('#datepicker').val($(td[2]).text());
+    $('#timepicker').val($(td[3]).text());
     // })
 });
 
 // Delete Button Done!!!
 $('.btn-floating.red').on('click', function () {
-    $(this).parents('tr').remove();
+    
 })
+
+function deleteData(index){
+    var pacientesList;
+    //pacientesList = Jso
+}
 
 
 /* date */
 $(function () {
     $("#datepicker").datepicker();
 });
-
+/* Timer */
 /* $(function() {
   $('#timepicker').timepicker();
 }); */
-/* Timer */
 
-//Time Picker:
+
 /* $('.timepicker').pickatime({
     default: 'now',
     twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
@@ -188,62 +121,3 @@ $(function () {
     autoclose: false,
     vibrate: true // vibrate the device when dragging clock hand
 }); */
-
-/* M.Datepicker.init(document.querySelectorAll(".datepicker"), {
-  format: "dd-mm-yyyy",
-  showClearBtn: true,
-  onClose: function() {
-    var newDate = $(this.el).parent().find('.datepicker').val();
-    $(this.el).parent().find('input[type!=hidden]').val(newDate);          
-  }
-});
-$(".datepicker-prefix .prefix").click(function() {
-  $(this)
-    .parent()
-    .find(".datepicker")
-    .datepicker("open");
-});
-$(".datepicker-prefix")
-  .find("input[type!=hidden]")
-  .change(function() {
-    if ($(this).val() != "") {
-      var comps = $(this)
-        .val()
-        .split("-");
-      // change code below to match your format needs
-      var date = new Date(
-        parseInt(comps[2]),
-        parseInt(comps[1]) - 1,
-        parseInt(comps[0])
-      );
-      $(this)
-        .parent()
-        .find(".datepicker")
-        .datepicker("setDate", date);
-    }
-  });
- 
-M.Timepicker.init(document.querySelectorAll(".timepicker"), {
-  twelveHour: false, // this feature doesn't work on 12-hour picker
-  showClearBtn: true,
-  onCloseEnd: function() {
-    var newTime = $(this.el).parent().find('.timepicker').val();
-    $(this.el).parent().find('input[type!=hidden]').val(newTime);          
-  }
-});
-$(".timepicker-prefix .prefix").click(function() {
-  $(this)
-    .parent()
-    .find(".timepicker")
-    .timepicker("open");
-});
-$(".timepicker-prefix")
-  .parent()
-  .find("input[type!=hidden]")
-  .change(function() {
-    $(this)
-      .parent()
-      .find(".timepicker")
-      .val($(this).val());
-  });
- */
